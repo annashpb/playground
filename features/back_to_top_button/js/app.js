b33823f6ce95fddb1820b0ff2b2c517f64896dec
Anna Shliakhova <anna.shliakhova@raketech.com>
2020-04-20 16:09:20 +0300

    back to top btn

---
+++ features/back_to_top_button/js/app.js
@@ -0,0 +1,20 @@
+"use strict";
+
+function backToTop() {
+  let button = $(".back-to-top");
+
+  $(window).on("scroll", () => {
+    if ($(window).scrollTop() >= 50) {
+      button.fadeIn();
+    } else {
+      button.fadeOut();
+    }
+  });
+
+  button.on("click", (e) => {
+    e.preventDefault();
+    $("html").animate({ scrollTop: 0 }, 1000);
+  });
+}
+
+backToTop();
