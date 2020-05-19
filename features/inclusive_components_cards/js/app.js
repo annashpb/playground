0034a215e59e31aa89263399a3a79a07a97f2cae
Anna <anna.sh.pb@gmail.com>
2020-05-20 00:24:41 +0300

    modified cards styles and added a card click handler

--- features/inclusive_components_cards/js/app.js
+++ features/inclusive_components_cards/js/app.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const cards = document.querySelectorAll(".card-js");
+
+Array.prototype.forEach.call(cards, (card) => {  
+  card.style.cursor = "pointer";
+  let down,
+    up,
+    link = card.querySelector("h2 a");
+  card.onmousedown = () => (down = +new Date());
+  card.onmouseup = () => {
+    up = +new Date();
+    if (up - down < 200 && event.target !== card.querySelector(".card__bottom--trailer")) {
+      link.click();      
+    }
+  };
+});
