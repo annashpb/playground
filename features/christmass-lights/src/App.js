380e589f48122d158b112fca1ad9ac453e112680
unknown <anna.sh.pb@gmail.com>
2021-12-03 14:34:07 +0200

    code cleanup

--- features/christmass-lights/src/App.js
+++ features/christmass-lights/src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 import Lights from './components/Lights';
 
 const App = () => {
-  return (
-    <div className="appWrapper">
-      <div className="headingContainer">
-        <h1 className="heading">Stranger things</h1>
-      </div>
-      <Lights />
-    </div>
-  );
-}
+	return (
+		<div className="appWrapper">
+			<div className="headingContainer">
+				<h1 className="heading">Stranger things</h1>
+			</div>
+			<Lights />
+		</div>
+	);
+};
 
 export default App;
